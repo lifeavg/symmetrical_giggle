9553371d3d6c9dbc71a0397d014fcee4dae61fa3
Artem <44943333+lifeavg@users.noreply.github.com>
2023-02-25 12:36:14 +0300

    connect test db

--- symmetrical_giggle/symmetrical_giggle/settings.py
+++ symmetrical_giggle/symmetrical_giggle/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'symmetrical_giggle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'symmetrical_giggle', 
+        'USER': 'postgres', 
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
