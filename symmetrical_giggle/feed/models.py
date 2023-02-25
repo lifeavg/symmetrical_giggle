b5ddb48fbabd454886412584902e5d46f2516830
Artem <44943333+lifeavg@users.noreply.github.com>
2023-02-25 13:30:12 +0300

    add post model

--- symmetrical_giggle/feed/models.py
+++ symmetrical_giggle/feed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    header = models.CharField(max_length=100)
+    # slug = models.SlugField(max_length=150, unique=True)
+    content = models.TextField(max_length=1000)
+    created = models.DateTimeField(verbose_name="date published")
+    # author
